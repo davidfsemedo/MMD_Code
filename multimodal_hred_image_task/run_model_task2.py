bc915de6836f7cf0b83a26aa76b6d6c2b101192f
MrLucasFischer <lucasfischerpt@gmail.com>
2019-04-12 12:32:05 +0100

    Adding documentation in run_model_task2

--- multimodal_hred_image_task/run_model_task2.py
+++ multimodal_hred_image_task/run_model_task2.py
@@ -48,6 +48,9 @@ def check_dir(param):
 
 
 def get_loss(cosine_sim_pos, cosine_sim_negs, mask_negs):
+    """
+    Calculate the difference in the positive and negative cosine similarities
+    """
     cosine_diff = [(x - cosine_sim_pos).tolist() for x in cosine_sim_negs]
     cosine_diff = np.asarray(cosine_diff)
 
@@ -70,7 +73,7 @@ def run_training(param):
     :param param: The dictionary object containing the system parameters for the model (obtained from params_v*.get_params())
     """
 
-    def get_train_loss(model, batch_dict, step):
+    def get_train_loss(model, batch_dict):
         """
         This method starts by obtaining the batch data necessary to feed to the model
         After obtaining the feed dictionary it feeds it to the model, running the training operation
@@ -82,32 +85,48 @@ def run_training(param):
             param['max_len'], param['max_images'], param['image_rep_size'], param['max_utter'], param['max_negs'],
             param['batch_size'], batch_dict)
 
-        # Create the
+        # Create the feed dictionary to be fed to the model
         feed_dict = feeding_dict(model, train_batch_text, train_batch_image, batch_image_target_pos,
                                  batch_image_target_negs, batch_mask_negs)
 
+        # Running the actual training operation, obtain the loss values for this batch
         batch_mm_loss, batch_cosine_sim_pos, batch_cosine_sim_negs, _ = sess.run(
             [loss, cosine_sim_pos, cosine_sim_negs, train_op], feed_dict=feed_dict)
 
         return batch_mm_loss, batch_cosine_sim_pos, batch_cosine_sim_negs, batch_mask_negs
 
     def get_valid_loss(model, batch_dict):
+        """
+        This method starts by obtaining the batch data necessary to feed to the model
+        After obtaining the feed dictionary it feeds it to the model, running validation operation
+        :param model: The model in which to run the operations
+        :param batch_dict: A batch of the validation data in which to extract the desired information to build a feed dictionary
+        :return: The output of the loss and logits operations for the validation set
+        """
+
+        # Obtain the variables needed to create the feed dictionary from this batch
         valid_batch_text, valid_batch_image, batch_image_target_pos, batch_image_target_negs, batch_mask_negs = get_batch_data(
             param['max_len'], param['max_images'], param['image_rep_size'], param['max_utter'], param['max_negs'],
             param['batch_size'], batch_dict)
+
+        # Create the feed dictionary to be fed to the model
         feed_dict = feeding_dict(model, valid_batch_text, valid_batch_image, batch_image_target_pos,
                                  batch_image_target_negs, batch_mask_negs)
+
+        # Running the actual training operation, obtain the loss values for this batch
         batch_mm_loss, batch_cosine_sim_pos, batch_cosine_sim_negs = sess.run([loss, cosine_sim_pos, cosine_sim_negs],
                                                                               feed_dict=feed_dict)
+
         return batch_mm_loss, batch_cosine_sim_pos, batch_cosine_sim_negs, batch_mask_negs
 
-    def get_sum(x):
-        return np.sum(np.asarray(x))
+    def evaluate(model, valid_data):
+        """
+        Performs the training procedure in the validation set every 10000 training steps
+        """
 
-    def evaluate(model, epoch, step, valid_data):
         print('Validation started')
-
         sys.stdout.flush()
+
         sum_valid_loss = 0.0
         sum_mm_loss = 0.0
         sum_cosine_sim_pos = 0.0
@@ -117,22 +136,30 @@ def run_training(param):
         n_batches = int(math.ceil(float(len(valid_data)) / float(param['batch_size'])))
 
         for i in range(n_batches):
+            # Obtain a batch from the validation set
             batch_dict = valid_data[i * param['batch_size']:(i + 1) * param['batch_size']]
+
+            # Perform the actual training process in the validation set
             batch_mm_loss, batch_cosine_sim_pos, batch_cosine_sim_negs, batch_mask_negs = get_valid_loss(model,
                                                                                                          batch_dict)
 
+            # Gets the cosine difference between the positive and negative cosine similarities
             batch_valid_loss, count_batch = get_loss(batch_cosine_sim_pos, batch_cosine_sim_negs, batch_mask_negs)
 
             batch_cosine_sim_negs = np.asarray(batch_cosine_sim_negs)
             avg_batch_cosine_sim_negs = np.average(batch_cosine_sim_negs, axis=0)
 
+            # sum up the cosine similarities
             sum_cosine_sim_pos = sum_cosine_sim_pos + np.sum(batch_cosine_sim_pos)
             sum_cosine_sim_neg = sum_cosine_sim_neg + np.sum(avg_batch_cosine_sim_negs)
 
+            # sum up the validation loss
             sum_valid_loss = sum_valid_loss + batch_valid_loss
             sum_mm_loss = sum_mm_loss + batch_mm_loss
+
             count_valid = count_valid + count_batch
 
+        # Average all the values
         avg_mm_loss = sum_mm_loss / float(len(valid_data))
         avg_cosine_sim_pos = sum_cosine_sim_pos / float(len(valid_data))
         avg_cosine_sim_neg = sum_cosine_sim_neg / float(len(valid_data))
@@ -140,14 +167,17 @@ def run_training(param):
 
         return avg_valid_loss, avg_mm_loss, avg_cosine_sim_pos, avg_cosine_sim_neg
 
+    # Loading the training data
     train_data = pkl.load(open(param['train_data_file'], "rb"))
     print('Train dialogue dataset loaded')
     sys.stdout.flush()
 
+    # Loading the validation data
     valid_data = pkl.load(open(param['valid_data_file'], "rb"))
     print('Valid dialogue dataset loaded')
     sys.stdout.flush()
 
+    # Loading the vocabolary file
     vocab = pkl.load(open(param['vocab_file'], "rb"))
     vocab_size = len(vocab)
     param['decoder_words'] = vocab_size
@@ -166,8 +196,11 @@ def run_training(param):
     print('number of batches ', n_batches, 'len train_data', len(train_data), ' batch size', param['batch_size'])
 
     model_file = os.path.join(param['model_path'], "best_model")
+
+    # Initiating the tensorflow graph
     with tf.Graph().as_default():
 
+        # Instantiating the model with the desired parameters
         model = Hierarchical_seq_model('image', param['text_embedding_size'], param['image_embedding_size'],
                                        param['image_rep_size'], param['cell_size'], param['cell_type'],
                                        param['batch_size'], param['learning_rate'], param['max_len'],
@@ -175,11 +208,13 @@ def run_training(param):
                                        param['decoder_words'], param['max_gradient_norm'], param['activation'],
                                        param['output_activation'])
 
+        # Creating the tensorflow placeholders to feed the model
         model.create_placeholder()
         output_representation = model.inference()
 
         loss, cosine_sim_pos, cosine_sim_negs = model.loss_task_image(output_representation)
 
+        # the actual training operation
         train_op, gradients = model.train(loss)
 
         print("model created")
@@ -189,6 +224,7 @@ def run_training(param):
         init = tf.initialize_all_variables()
         sess = tf.Session()
 
+        # Restoring to a previous best model if it exists
         if os.path.isfile(model_file):
             print("best model exists.. restoring from that point")
             saver.restore(sess, model_file)
@@ -204,6 +240,7 @@ def run_training(param):
         for var in all_var:
             print(var.name, var.get_shape())
 
+        # The training process starts here
         print('training started')
         sys.stdout.flush()
         last_avg_mm_loss = None
@@ -212,6 +249,7 @@ def run_training(param):
         for epoch in range(param['max_epochs']):
             r = random.random()
             random.shuffle(train_data, lambda: r)
+
             sum_cosine_sim_pos = 0.0
             sum_cosine_sim_neg = 0.0
             count_train = 0
@@ -226,20 +264,30 @@ def run_training(param):
                     sys.stdout.flush()
 
                 overall_step_count = overall_step_count + 1
+
+                # Get a batch of the data from the training set
                 train_batch_dict = train_data[i * param['batch_size']:(i + 1) * param['batch_size']]
+
+                # Run the actual training operation for this batch
                 batch_mm_loss, batch_cosine_sim_pos, batch_cosine_sim_negs, batch_mask_negs = get_train_loss(model,
-                                                                                                             train_batch_dict,
-                                                                                                             overall_step_count)
+                                                                                                             train_batch_dict)
+
+                # Calculate the difference in the positive and negative cosine similarities
                 batch_train_loss, count_batch = get_loss(batch_cosine_sim_pos, batch_cosine_sim_negs, batch_mask_negs)
+
                 batch_cosine_sim_negs = np.asarray(batch_cosine_sim_negs)
+
                 avg_batch_cosine_sim_negs = np.average(batch_cosine_sim_negs, axis=0)
+
                 sum_batch_cosine_sim_pos = np.sum(batch_cosine_sim_pos)
                 sum_batch_cosine_sim_neg = np.sum(avg_batch_cosine_sim_negs)
+
                 sum_cosine_sim_pos = sum_cosine_sim_pos + sum_batch_cosine_sim_pos
                 sum_cosine_sim_neg = sum_cosine_sim_neg + sum_batch_cosine_sim_neg
                 sum_train_loss = sum_train_loss + batch_train_loss
                 sum_mm_loss = sum_mm_loss + batch_mm_loss
                 count_train = count_train + count_batch
+
                 if overall_step_count % param['print_train_freq'] == 0:
                     print(
                         'Epoch  %d Step %d average train loss is =%.6f   average MM loss =%.6f (cosine-dist-pos=%.6f cosine-dist-neg=%.6f) over %d instances' % (
@@ -248,18 +296,24 @@ def run_training(param):
                             sum_batch_cosine_sim_pos / float(len(train_batch_dict)),
                             sum_batch_cosine_sim_neg / float(len(train_batch_dict)), len(train_batch_dict)))
                     sys.stdout.flush()
+
+                # At every 10000 steps run the training process in the validation set
                 if overall_step_count > 0 and overall_step_count % param['valid_freq'] == 0:
-                    avg_valid_loss, avg_mm_loss, avg_cosine_sim_pos, avg_cosine_sim_neg = evaluate(model, epoch, i,
-                                                                                                   valid_data)
+
+                    # Running the actual training process in the validation set
+                    avg_valid_loss, avg_mm_loss, avg_cosine_sim_pos, avg_cosine_sim_neg = evaluate(model, valid_data)
                     print(
                         'Epoch %d Step %d average valid loss is =%.6f   average MM loss =%.6f (cosine-dist-pos=%.6f cosine-dist-neg=%.6f) ' % (
                             epoch, i, avg_valid_loss, avg_mm_loss, avg_cosine_sim_pos, avg_cosine_sim_neg))
                     sys.stdout.flush()
+
+                    # If the model gave a better result in the validation set than before, save this model as the best model
                     if best_valid_loss > avg_valid_loss:
                         saver.save(sess, model_file)
                         best_valid_loss = avg_valid_loss
                     else:
                         continue
+
             avg_mm_loss = sum_mm_loss / float(len(train_data))
             avg_train_loss = sum_train_loss / float(count_train)
             avg_cosine_sim_pos = sum_cosine_sim_pos / float(len(train_data))
@@ -268,6 +322,7 @@ def run_training(param):
                 'Epoch %d of training is completed ... average train loss is =%.6f   average MM loss =%.6f (cosine-dist-pos=%.6f cosine-dist-neg=%.6f) ' % (
                     epoch, avg_train_loss, avg_mm_loss, avg_cosine_sim_pos, avg_cosine_sim_neg))
             sys.stdout.flush()
+
             if last_avg_mm_loss is not None and avg_mm_loss > last_avg_mm_loss:
                 diff = avg_mm_loss - last_avg_mm_loss
                 if diff > param['train_loss_incremenet_tolerance']:
@@ -280,8 +335,10 @@ def run_training(param):
                             avg_mm_loss, diff, param['train_loss_incremenet_tolerance']))
             last_avg_mm_loss = avg_mm_loss
             sys.stdout.flush()
+
         print('Training over')
         print('Evaluating on test data')
+
     f_out.close()
 
 
